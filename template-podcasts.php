bd20aaa6c02c0dec5a7824005bd73e28df6cd541
sngjss <sunga.jss@gmail.com>
2015-04-21 09:49:04 -0400

    Added test styling on podcasters section for possible viewing/filtering. Subject to change.

--- template-podcasts.php
+++ template-podcasts.php
@@ -35,9 +35,15 @@ get_header();?>
 		</article>
 		<article class="podcasters">
 			<h2> Podcasters </h2>
-			<h3> Featured </h3>
 			<!-- Tentative Feature, to be determined whether or not to keep. Keep if filtering option is going to be drop-down. Or have a link to a separate page for podcasters. -->
 			<div class="featured-wrapper">
+				<!-- Test -->
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
+				<!-- Test -->
 			</div>
 		</article>
 	</aside>
