33a1f016957afbb3dfea6dead42369339d828358
sngjss <sunga.jss@gmail.com>
2015-04-21 11:08:57 -0400

    Changed div name from featured-wrapper to podcasters-wrapper.

--- template-podcasts.php
+++ template-podcasts.php
@@ -36,7 +36,7 @@ get_header();?>
 		<article class="podcasters">
 			<h2> Podcasters </h2>
 			<!-- Tentative styling. We can have a dropdown for podcasters or use just a basic list of them. Or have a link to a separate page for podcasters. -->
-			<div class="featured-wrapper">
+			<div class="podcasters-wrapper">
 				<!-- Test -->
 				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
 				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. HEY HEY HEY WHERE IS DAH OVERFLOW~~~~!!!!"</p>
