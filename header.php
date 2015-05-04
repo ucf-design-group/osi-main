181a5c9b4db24ecee5ec4b5dabd44e7f34baba3c
Jake Ferrante <jake.ferrante@hotmail.com>
2015-05-04 09:51:21 -0400

    Adds header links back (oops I deleted them cause whitespace error).

--- header.php
+++ header.php
@@ -98,5 +98,15 @@
 				</nav>
 
 				<div class="agencies">
+					<a target="_blank" href="<?php echo get_site_url()?>/cab/">CAB</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/homecoming/">Homecoming</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/knight-camp/">Knight Camp</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/knight-thon/">Knight Thon</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/kort/">KoRT</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/late-knights/">Late Knights</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/sos/">SOS</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/vucf/">Volunteer UCF</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/rosenlife/">Rosen Life</a>
+					<a target="_blank" href="<?php echo get_site_url()?>/creativeservices/">Design Group</a>
 				</div>
 			</header>
