65ead44fb35e44b50b02b8b912b0d33983bc174c
Jake Ferrante <jake.ferrante@hotmail.com>
2015-05-05 12:14:03 -0400

    Updates some fields to <option> tags

--- template-podcaster-application.php
+++ template-podcaster-application.php
@@ -38,6 +38,11 @@ include(locate_template('partials/single-podcaster-application-handler.php'));
 						<input type="text" name="podcaster-application-form-lname" value="">
 					</p>
 					<p>
+						<label for='podcaster-application-form-ucfid'>UCF ID</label>
+						<br>
+						<input type="text" name="podcaster-application-form-lname" value="">
+					</p>
+					<p>
 						<label for='podcaster-application-form-major'>Major</label>
 						<br>
 						<input type="text" name="podcaster-application-form-major" value="">
@@ -45,7 +50,13 @@ include(locate_template('partials/single-podcaster-application-handler.php'));
 					<p>
 						<label for='podcaster-application-form-year'>Year</label>
 						<br>
-						<input type="text" name="podcaster-application-form-year" value="">
+						<select name="podcaster-application-form-year">
+							<option value="Freshman">Freshman</option>
+							<option value="Sophomore">Sophomore</option>
+							<option value="Junior">Junior</option>
+							<option value="Senior+">Senior+</option>
+							<option value="Graduate">Graduate</option>
+						</select>
 					</p>
 					<p>
 						<label for='podcaster-application-form-gpa'>GPA</label>
@@ -60,6 +71,12 @@ include(locate_template('partials/single-podcaster-application-handler.php'));
 						<small>How many podcasts you think you will create.</small>
 						<br>
 						<input type="text" name="podcaster-application-form-num-podcasts" value="">
+						<select name="podcaster-application-form-num-podcasts">
+							<option value="1-5">1-5</option>
+							<option value="6-15">6-15</option>
+							<option value="16-25">16-25</option>
+							<option value="26+">26+</option>
+						</select>
 					</p>
 					<p>
 						<label for='podcaster-application-form-frequency'>Frequency of Podcasts</label>
@@ -67,7 +84,11 @@ include(locate_template('partials/single-podcaster-application-handler.php'));
 						<small>How often you think you'll be uploading podcasts</small>
 						<small><i>How often you think you'll be uploading podcasts</i></small>
 						<br>
-						<input type="text" name="podcaster-application-form-frequency" value="">
+						<select name="podcaster-application-form-frequency">
+							<option value="1-2">(1-2)/month</option>
+							<option value="3-10">(3-10)/month</option>
+							<option value="11+">(10+)/month</option>
+						</select>
 					</p>
 					<p>
 						<label for='podcaster-application-form-podcast-length'>Length of Podcasts</label>
@@ -75,6 +96,12 @@ include(locate_template('partials/single-podcaster-application-handler.php'));
 						<small>How long you think each podcast will be.</small>
 						<br>
 						<input type="text" name="podcaster-application-form-podcast-length" value="">
+						<select name="podcaster-application-form-podcasts-length">
+							<option value="1-2">1-2 minutes</option>
+							<option value="3-10">3-10 minutes</option>
+							<option value="11-20">11-20 mintes</option>
+							<option value="21+">21 mintes</option>
+						</select>
 					</p>
 					<p>
 						<label for='podcaster-application-form-podcast-description'>Podcasts Description</label>
