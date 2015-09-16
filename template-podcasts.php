d036bb0006a3665597f6623c03b700922730cc1e
sngjss <sunga.jss@gmail.com>
2015-09-16 08:51:22 -0400

    Small edits to podcasts-- some deletion for tests.

--- template-podcasts.php
+++ template-podcasts.php
@@ -38,51 +38,21 @@ get_header();?>
 			<h2> Podcasters </h2>
 			<!-- Tentative styling. We can have a dropdown for podcasters or use just a basic list of them. Or have a link to a separate page for podcasters. -->
 			<!-- In podcasters-wrapper, it contains a collection of the podcasters that are contributing to the website. On click of the single item in the collection, the page of podcasters will be filtered to display content only from that podcaster. -->
+			<!-- A single podcaster item will contain their profile image and name (maybe a small podcast description) -->
 			<div class="podcasters-wrapper">
 				<!-- Test -->
-					<figure class="single-wrapper">
-						<div class="podcaster-thumb-wrapper">
-							<?php echo get_the_post_thumbnail($post->ID, 'post-thumbnail'); ?>
-						</div>
-						<div class="description">
-							<h4><?php the_author(); ?></h4>
-							<p> Supporting podcasters on UCF Campus.</p>
-						</div>
-					</figure>
-				<!-- A single podcaster item will contain their profile image and name (maybe a small podcast description) -->
+				<figure class="single-wrapper">
+					<div class="podcaster-thumb-wrapper">
+						<?php echo get_the_post_thumbnail($post->ID, 'post-thumbnail'); ?>
+					</div>
+					<div class="description">
+						<h4><?php the_author(); ?></h4>
+						<p> Supporting podcasters on UCF Campus.</p>
+					</div>
+				</figure>
 			</div>
 		</article>
 	</aside>
 </div>
-
-<script type="text/javascript">
-
-	var count = 2;
-	var total = <?php echo $wp_query->max_num_pages; ?>;
-
-	$(window).scroll(function(){
-		if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-			if (count > total) {
-				return false;
-			} else {
-				loadArticle(count);
-				count++;
-			}
-		}
-	});
-
-	function loadArticle(pageNumber) {
-		$.ajax({
-			url: "<?php bloginfo('wpurl') ?>/wp-admin/admin-ajax.php",
-			type:'POST',
-			data:"action=inifinite_scroll&page_no=" + pageNumber + '&loop_file=loop',
-			success: function(html) {
-				$(".main").append(html);
-			}
-		});
-		return false;
-	}
-
-</script>
 
 <?php get_footer(); ?>
